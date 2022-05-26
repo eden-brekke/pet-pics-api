5991a8df4a08330b7ab943a81d731bcfc4cbf683
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-26 15:24:36 -0700

    added allowed host

--- pet_pics_api_project/settings.py
+++ pet_pics_api_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7u#(6c-tcd^frj_4jk5#f#scg2t3t-6(!tekq&ncc-tupklpt@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1',]
 
 
 # Application definition
