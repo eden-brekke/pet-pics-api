ca9e83db8ed84ef78bf8793a8d84b54a6db467ae
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-26 14:38:02 -0700

    added model with fields

--- pet_pics/models.py
+++ pet_pics/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+class PetPic(models.Model):
+    name = models.CharField(max_length=256)
+    type_of_pet = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    img = models.URLField(max_length=256)
+    added_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
